710f3871e1bc237799590770add60005859f9000
Rasmote <workrasmote@gmail.com>
2025-07-22 12:53:12 +0900

    hello띄우기
    성공

--- src/main/java/rasmote/github/io/ai_diary/controller/DiaryController.java
+++ src/main/java/rasmote/github/io/ai_diary/controller/DiaryController.java
@@ -1,5 +1,13 @@
 package rasmote.github.io.ai_diary.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class DiaryController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello, User!";
+    }
 
 }
